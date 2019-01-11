fc695b9f60827750aa02e888338952a86a18049f
valmrt77 <36480710+valmrt77@users.noreply.github.com>
2019-01-11 14:50:35 +0100

    Fix FOS User Bundle

--- bookest/src/Entity/Users.php
+++ bookest/src/Entity/Users.php
@@ -1,160 +1,27 @@
 <?php
+// src/AppBundle/Entity/Users.php
 
 namespace App\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-use FOS\UserBundle\Model\UserInterface;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ * @ORM\Entity
+ * @ORM\Table(name="users")
  */
-class Users implements UserInterface
+class Users extends BaseUser
 {
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
+     * @ORM\Id
      * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $username;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $usernameCanonical;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $email;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $emailCanonical;
-
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $enabled;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $salt;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $password;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $confirmationToken;
-
-    public function getId(): ?int
+    public function __construct()
     {
-        return $this->id;
-    }
-
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
-
-    public function setUsername($username): self
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    public function getUsernameCanonical(): ?string
-    {
-        return $this->usernameCanonical;
-    }
-
-    public function setUsernameCanonical($usernameCanonical): self
-    {
-        $this->usernameCanonical = $usernameCanonical;
-
-        return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail($email): self
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    public function getEmailCanonical(): ?string
-    {
-        return $this->emailCanonical;
-    }
-
-    public function setEmailCanonical($emailCanonical): self
-    {
-        $this->emailCanonical = $emailCanonical;
-
-        return $this;
-    }
-
-    public function getEnabled(): ?bool
-    {
-        return $this->enabled;
-    }
-
-    public function setEnabled($enabled): self
-    {
-        $this->enabled = $enabled;
-
-        return $this;
-    }
-
-    public function getSalt(): ?string
-    {
-        return $this->salt;
-    }
-
-    public function setSalt($salt): self
-    {
-        $this->salt = $salt;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword($password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    public function getConfirmationToken(): ?string
-    {
-        return $this->confirmationToken;
-    }
-
-    public function setConfirmationToken($confirmationToken): self
-    {
-        $this->confirmationToken = $confirmationToken;
-
-        return $this;
+        parent::__construct();
+        // your own logic
     }
 }
