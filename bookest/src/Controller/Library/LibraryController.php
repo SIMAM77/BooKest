99066873ec2877496da10cbebe01cef9c73ba924
SIMAM77 <souleimane.imam@hetic.net>
2019-01-29 17:41:23 +0100

    recherche ok

--- bookest/src/Controller/Library/LibraryController.php
+++ bookest/src/Controller/Library/LibraryController.php
@@ -111,6 +111,56 @@ class LibraryController extends Controller
         ));
     }
   
+    // public function BookListPage()
+    // {
+    //     $a_category = ['Arts & Music', 'Biographies', 'Business', 'Kids', 'Comics', 'Computers', 'Cooking' ];
+    //     $s_secret = "AIzaSyAhuwOyHCANRqaNa66WtUdyrfFtK2-7S9M";
+
+    //     $a_url = [];
+    //     $a_datas = [];
+    //     foreach($a_category as $api_parameter){
+    //         $url="https://www.googleapis.com/books/v1/volumes?q=subject:" . $api_parameter . "&maxresults=10&langRestrict=fr&key=" . $s_secret . "";
+    //         $curl = curl_init();
+    //         $opts = [
+    //             CURLOPT_URL => $url,
+    //             CURLOPT_RETURNTRANSFER => true,
+    //         ];
+    //         curl_setopt_array($curl, $opts);
+    //         $response = curl_exec($curl);
+    //         curl_close($curl);
+    
+    //         $res = json_decode($response);
+    //         if(!empty($res)){
+    //             $a_url[] = $res;
+    //         }
+
+    //         $a_book = [];
+    //         foreach ($a_url as $category) {
+    //             if (!empty($category) && !empty($category->items)) {
+    //                 dump($category);
+    //                 foreach($category->items as $book){    
+    //                     if(!empty($book->volumeInfo->categories) && in_array($api_parameter, $book->volumeInfo->categories)){                      
+                                            
+    //                         $a_book[] = [
+    //                             'titre'       => !empty($book->volumeInfo->title) ? $book->volumeInfo->title : 'null',
+    //                             'auteur'      => !empty($book->volumeInfo->authors) ? $book->volumeInfo->authors : 'null',
+    //                             'description' => !empty($book->volumeInfo->description) ? $book->volumeInfo->description : 'null',
+    //                             'editeur'     => !empty($book->volumeInfo->publisher) ? $book->volumeInfo->publisher : 'null',
+    //                             'genre'       => !empty($book->volumeInfo->categories) ? $book->volumeInfo->categories : 'null',
+    //                             'publication' => !empty($book->volumeInfo->publishedDate) ? $book->volumeInfo->publishedDate : 'null',
+    //                             'isbn'        => !empty($book->volumeInfo->industryIdentifiers[0]->identifier) ? $book->volumeInfo->industryIdentifiers[0]->identifier : 'null',
+    //                         ];
+    //                         $a_datas[$api_parameter] = $a_book;
+    //                     }          
+    //                 }
+    //             }          
+    //         }
+    //     }
+    //     return $this->render('Library/catalogue.html.twig', [
+    //         'elements' => $a_datas
+    //     ]
+    //     );
+    // }
     public function BookListPage()
     {
         $a_category = ['Arts & Music', 'Biographies', 'Business', 'Kids', 'Comics', 'Computers', 'Cooking' ];
@@ -119,7 +169,7 @@ class LibraryController extends Controller
         $a_url = [];
         $a_datas = [];
         foreach($a_category as $api_parameter){
-            $url="https://www.googleapis.com/books/v1/volumes?q=subject:" . $api_parameter . "&maxresults=10&key=" . $s_secret . "";
+            $url="https://www.googleapis.com/books/v1/volumes?q=subject:" . $api_parameter . "&maxresults=10&langRestrict=fr&key=" . $s_secret . "";
             $curl = curl_init();
             $opts = [
                 CURLOPT_URL => $url,
@@ -136,7 +186,8 @@ class LibraryController extends Controller
 
             $a_book = [];
             foreach ($a_url as $category) {
-                if (!empty($category)) {
+                if (!empty($category) && !empty($category->items)) {
+                    dump($category);
                     foreach($category->items as $book){    
                         if(!empty($book->volumeInfo->categories) && in_array($api_parameter, $book->volumeInfo->categories)){                      
                                             
@@ -157,6 +208,55 @@ class LibraryController extends Controller
         }
         return $this->render('Library/catalogue.html.twig', [
             'elements' => $a_datas
+        ]
+        );
+    }
+    
+    public function searchBook(Request $request)
+    {
+        $search = $request->request->all();
+        $a_category = ['Arts & Music', 'Biographies', 'Business', 'Kids', 'Comics', 'Computers', 'Cooking' ];    
+        $a_url = [];
+        $a_datas = [];    
+        $a_book = [];
+        if(!empty($search)){
+            $book = $search['isbn'];
+            $key = "AIzaSyAhuwOyHCANRqaNa66WtUdyrfFtK2-7S9M";
+            $url="https://www.googleapis.com/books/v1/volumes?q=".$book."&key=".$key."";
+            $curl = curl_init();
+            $opts = [
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+            ];
+            curl_setopt_array($curl, $opts);
+            $response = curl_exec($curl);
+            curl_close($curl);
+    
+            $res = json_decode($response);
+
+            foreach($res->items as $book){
+                // if(!empty($book->volumeInfo->categories) && in_array($api_parameter, $book->volumeInfo->categories)){                      
+                if(!empty($book->volumeInfo->categories)){                      
+                                    
+                    $a_book[] = [
+                        'titre'       => !empty($book->volumeInfo->title) ? $book->volumeInfo->title : 'null',
+                        'auteur'      => !empty($book->volumeInfo->authors) ? $book->volumeInfo->authors : 'null',
+                        'description' => !empty($book->volumeInfo->description) ? $book->volumeInfo->description : 'null',
+                        'editeur'     => !empty($book->volumeInfo->publisher) ? $book->volumeInfo->publisher : 'null',
+                        'genre'       => !empty($book->volumeInfo->categories) ? $book->volumeInfo->categories : 'null',
+                        'publication' => !empty($book->volumeInfo->publishedDate) ? $book->volumeInfo->publishedDate : 'null',
+                        'isbn'        => !empty($book->volumeInfo->industryIdentifiers[0]->identifier) ? $book->volumeInfo->industryIdentifiers[0]->identifier : 'null',
+                    ];
+                    
+                }          
+    //             }          
+    
+            }
+        }
+        
+        dump($a_book);
+        return $this->render('Library/search.html.twig', [
+            'elements' => $a_book
         ]
         );
     }
