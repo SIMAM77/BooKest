b3b763b7e174217a6c6509d7025994962205e4e5
valmrt77 <36480710+valmrt77@users.noreply.github.com>
2019-03-15 09:57:15 +0100

    add books api

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -8,9 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
 use App\Entity\Users;
+use App\Entity\Livre;
 
 class ApiController extends Controller
 {
+
+    // USER API CONTROLLER ----------------------------------------------------------------------------
 
     /**
      * Retrieves a user resource
@@ -42,6 +45,42 @@ class ApiController extends Controller
             return View::create("Il n'y a aucun utilisateur à afficher.", Response::HTTP_NOT_FOUND);
         } else {
             return View::create($o_user, Response::HTTP_OK);
+        }
+        
+    }
+
+    // BOOK API CONTROLLER ----------------------------------------------------------------------------
+
+    /**
+     * Retrieves a user resource
+     * @Rest\Get("/books/")
+     */
+    public function getBooks(): View
+    {
+        $o_book = $this->getDoctrine()->getRepository(Livre::class)->findAll();
+
+        if(empty($o_book)){
+            return View::create("Il n'y a aucun livre à afficher.", Response::HTTP_NOT_FOUND);
+        } else {
+            return View::create($o_book, Response::HTTP_OK);
+        }
+        
+    }
+
+    /**
+     * Retrieves a usr resource
+     * @Rest\Get("/book/{userId}")
+     */
+    public function getBook(int $bookId): View
+    {
+        $o_book = $this->getDoctrine()->getRepository(Livre::class)->findById($bookId);
+
+        $s_error = "Il n'y a aucune ville à afficher";
+
+        if(empty($o_book)){
+            return View::create("Il n'y a aucun livre à afficher.", Response::HTTP_NOT_FOUND);
+        } else {
+            return View::create($o_book, Response::HTTP_OK);
         }
         
     }
